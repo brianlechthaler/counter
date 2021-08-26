2be312812aa6888939c3738bb76eff0a861c7473
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-08-26 16:22:52 -0700

    ⚛️🧩🔢Create a React Component for our Counter

---
+++ src/ui/counter.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import BigBlueButton from '../ui/buttons';
+
+export default function Counter() {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        document.title = `Count: ${count}`
+    });
+    return(
+        <div>
+            <p>The current count is {count}.</p>
+            <BigBlueButton onClick={() => setCount(count + 1)}>Click to add 1 to counter.</BigBlueButton>
+
+        </div>
+    )
+}
